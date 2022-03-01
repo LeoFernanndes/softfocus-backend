0122a6f6dc26f0ce6ae083c79981b6e9689eb68c
LeoFernanndes <balbino.leonel@gmail.com>
2022-03-01 17:23:22 -0300

    add static files dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py makemigrations people loss_communications
 python manage.py migrate
 python manage.py loaddata users
 #gunicorn backend_python_wallet.wsgi:application --bind 0.0.0.0:8000 --workers 3 --access-logfile='-'
-python manage.py runserver
+python manage.py runserver 0.0.0.0:8000
 exec "$@"
